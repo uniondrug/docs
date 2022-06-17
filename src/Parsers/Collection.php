cd29a8285aab0d4d0818cb84f6c41c567877e483
kang.xiaoqiang <kangxiaoqiang@uniondrug.com>
2022-06-17 15:43:14 +0800

    no message

--- src/Parsers/Collection.php
+++ src/Parsers/Collection.php
@@ -244,11 +244,11 @@ class Collection extends Base
      */
     public function toTorna($upload = true)
     {
-        $this->console->info("\033[0:32m开始上传文档到Torna...\033[0m");
         $data = $this->toTornaData();
         if (!$upload) {
             return $data;
         }
+        $this->console->info("\033[0:32m开始上传文档到Torna...\033[0m");
         if (empty($this->tornaToken)) {
             $this->console->error("上传Torna出错: Missing Torna Access Token");
             return [];
